f21ee7f9231c3ae84a5527b084b7f55b8f0119bf
Daniel Wessel <djwessel@tum.de>
2017-01-28 18:53:58 +0100

    Added calendar to allow student to check Attendance information for given group. Also added switching groups.

--- AAT_gae/src/main/webapp/attendance.jsp
+++ AAT_gae/src/main/webapp/attendance.jsp
@@ -9,15 +9,21 @@
 
 <t:page>
   <jsp:attribute name="head">
+    <script src='//cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
     <script src="js/attendance.js"></script>
 
     <link rel="stylesheet" type="text/css" href="/css/attendance.css">
+    <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css' />
   </jsp:attribute>
   <jsp:attribute name="breadcrumbs">
     <li><a href="/">Home</a></li>
     <li class="active">Attendance Information</li>
   </jsp:attribute>
   <jsp:body>
-    <h1>Attendance Information</h1>
+    <h1>Loading Course Title...</h1>
+    <p id="attenCount">Loading Attendance Count...</p>
+    <p id="presentCount">Loading Presentation Count...</p>
+    <div id="calendar"></div>
   </jsp:body>
 </t:page>
--- AAT_gae/src/main/webapp/js/index.js
+++ AAT_gae/src/main/webapp/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Course Create form submit handler
   $('#courseCreate').submit(function() {
     $.post('/rest/course', $(this).serialize()).done(function(data) {
       window.location.reload();
@@ -9,6 +10,7 @@ $(document).ready(function() {
     return false;
   });
 
+  // Load courses
   $.get('/rest/courses', function(data) {
     data.forEach(function(course) {
       var cDiv = $('<a href="/course.jsp?courseId=' + course.id + '" class="course">' + course.title + '</a>').appendTo($('#courses'));
@@ -17,16 +19,15 @@ $(document).ready(function() {
 
   if (Cookies.get('user')) {
     if (Cookies.get("userType") === "student") {
+      // Load user's groups
       $.get('/rest/user/' + Cookies.get('user') + '/attendances', function(data) {
         for (var course in data) {
-          if (!data.hasOwnProperty(course)) continue;
-
           var rDiv = $('<a href="/attendance.jsp?groupId=' + data[course].id + '" class="record">' + course + ' (' + data[course].name + ')</a>').appendTo($('#records'));
         }
-      
       });
     }
     else {
+      // Show create course button
       $('#createCourseBtn').show().click(function() {
         $(this).hide();
         $('#courseCreate').show();
